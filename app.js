b80b102a7108ecf35c2031fabe64399a2c9b43f1
Gran Torino <red.chekist@gmail.com>
2020-05-03 17:28:28 +0300

    backend release

--- app.js
+++ app.js
@@ -1,6 +1,50 @@
 const express = require('express')
 const path = require('path')
+const {v4} = require('uuid')
 const app = express()
+
+app.use(express.json()) // <== поддержка сервером JSON
+
+// DB
+let CONTACTS = [
+    { id: v4(), marked: false, name:"John Doe", value:"value from DB" }
+]
+
+//***************************************
+//               ROUTES
+//***************************************
+
+// GET
+app.get('/api/contacts', (req, res) => {
+    // TODO: искусственный timeOut сервера
+    setTimeout(() => {
+        res.status(200).json(CONTACTS)
+    }, 1000)
+})
+
+// POST
+app.post('/api/contacts', (req, res) => {
+    const contact = {...req.body, id: v4(), marked: false}
+    CONTACTS.push(contact)
+    res.status(201).json(contact) // 201 елемент был создан
+})
+
+// DELETE
+app.delete('/api/contacts/:id', (req, res) => {
+    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
+    res.status(200).json({ message: 'OK' })
+})
+
+// PUT (полностью меняет модель)
+app.put('/api/contacts/:id', (req, res) => {
+    const idx = CONTACTS.findIndex(c => c.id === req.params.id)
+    CONTACTS[idx] = req.body
+    res.json(CONTACTS[idx]) // <== по умолчанию статус 200
+})
+
+// PATCH (частично меняет модель)...
+
+
 
 app.use(express.static(path.resolve(__dirname, 'client'))) // <== делаем папку статической
 
--- client/frontend.js
+++ client/frontend.js
@@ -1,9 +1,20 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
 
+Vue.component('loader', {
+    template: `
+    <div class="d-flex justify-content-center align-items-center p-5">
+        <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    `
+})
+
 new Vue({
     el: '#app',
     data() {
         return {
+            loading: false,
             form: {
                 name: '',
                 value: ''
@@ -19,20 +30,52 @@ new Vue({
     },
 
     methods: {
-        createContact() {
+        async createContact() {
             const {...contact} = this.form
 
-            this.contacts.push({...contact, id: Date.now(), marked: false})
+            const newContact = await request('/api/contacts', 'POST', contact)
+
+            this.contacts.push(newContact)
 
             this.form.name = this.form.value = ''
         },
-        markContact(id) {
-            console.log(id)
+        async markContact(id) {
             const contact = this.contacts.find(c => c.id === id)
-            contact.marked = true
+            const updated = await request (`/api/contacts/${id}`, 'PUT', {
+                ...contact,
+                marked: true
+            })
+            contact.marked = updated.marked
         },
-        removeContact(id) {
+        async removeContact(id) {
+            await request (`/api/contacts/${id}`, 'DELETE')
             this.contacts = this.contacts.filter(c => c.id !== id)
         }
+    },
+
+    async mounted() {
+        this.loading = true
+        this.contacts = await request('/api/contacts')
+        this.loading = false
     }
 })
+
+async function request(url, method = 'GET', data = null) {
+    try {
+        const headers = {}
+        let body
+
+        if (data) {
+            headers['Content-Type'] = 'application/json'
+            body = JSON.stringify(data)
+        }
+        const response = await fetch(url, {
+            method,
+            headers,
+            body
+        })
+        return await response.json()
+    } catch (e) {
+        console.warn('Error:', e.message)
+    }
+}
